1810cdbd90afaeeab29fabfaeb76f70baee81e59
Melvin Wilson <13534603+NarrowGate@users.noreply.github.com>
2019-12-24 15:09:08 +1000

    Add menu items
    Board, Diary, Studies and Contact

--- bi/Controllers/HomeController.cs
+++ bi/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace bi.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
