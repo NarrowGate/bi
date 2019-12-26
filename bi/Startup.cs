d6f09125d43846f9f67d6e8320f82ff09a949c10
Melvin Wilson <13534603+NarrowGate@users.noreply.github.com>
2019-12-26 12:25:41 +1000

    Add vue to layout

--- bi/Startup.cs
+++ bi/Startup.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace bi
@@ -41,6 +44,17 @@ namespace bi
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+
+            // Add support for node_modules but only during development **temporary**
+            if (env.IsDevelopment())
+            {
+                app.UseStaticFiles(new StaticFileOptions()
+                {
+                    FileProvider = new PhysicalFileProvider(
+                      Path.Combine(Directory.GetCurrentDirectory(), @"node_modules")),
+                    RequestPath = new PathString("/vendor")
+                });
+            }
 
             app.UseRouting();
 
