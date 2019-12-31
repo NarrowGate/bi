09a051351890fdce8ffac106e52cd25a2aad2407
Melvin Wilson <13534603+NarrowGate@users.noreply.github.com>
2019-12-31 12:10:01 +1000

    Add font awesome

--- bi/wwwroot/js/toDoList/to_do_list.js
+++ bi/wwwroot/js/toDoList/to_do_list.js
@@ -4,22 +4,33 @@ let todolist = {
     template: 
     `
         <div>
-            <a href="" @click.prevent="additem">Add item</a>
-            <input type="text" ref="addItem">
+            <div class="row">
+
+                <div class="col-sm-8 itemAddInp">
+                    <input type="text" ref="addinput">    
+                </div>
+
+                <div class="col-sm-4">
+                    <a href="" @click.prevent="add" class="addItem">
+                        <i class="fas fa-plus"></i>
+                    </a>            
+                </div>
+                
+            </div>                    
         
             <ul>
                 <li v-for="item in items">{{item}}</li>
             </ul>
         </div>
-        
+
     `,
     
     props: ['items'],
 
     methods: {
 
-        additem: function() {
-            this.$emit('newitem', this.$refs.addItem.value)
+        add:  function() {
+            this.$emit('newitem', this.$refs.addinput.value)
         }
 
     }
